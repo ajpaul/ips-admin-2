85b30e29f273e42f02745f82ea4eb82ce9491df4
Andrew Chumich <andrewchumich@gmail.com>
2016-08-17 14:09:52 -0600

    address and buildings form done

--- src/app/site-detail/site-detail.component.ts
+++ src/app/site-detail/site-detail.component.ts
@@ -1,10 +1,10 @@
-import { Component, ROUTER_DIRECTIVES, Input, ISite, MdInput } from './site-detail';
+import { Component, ROUTER_DIRECTIVES, Input, ISite, MdInput, MdCheckbox } from './site-detail';
 
 @Component({
     selector: 'app-site-detail',
     template: require('./site-detail.component.html'),
     styles: [require('./site-detail.component.less')],
-    directives: [ROUTER_DIRECTIVES, MdInput],
+    directives: [ROUTER_DIRECTIVES, MdInput, MdCheckbox],
 })
 
 export class SiteDetailComponent {
--- src/app/site-detail/site-detail.ts
+++ src/app/site-detail/site-detail.ts
@@ -5,3 +5,4 @@ export { ROUTER_DIRECTIVES } from '@angular/router';
 export { ISite, AllSitesService } from '../sites-all/sites-all.ts'
 export { Subscription } from 'rxjs/Subscription'
 export { MdInput } from '@angular2-material/input'
+export { MdCheckbox } from '@angular2-material/checkbox'
