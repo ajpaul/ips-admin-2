4aa875143c2e4bc6caea2868b74c6c2240b9776f
sxd15 <scott.davis@acuitybrands.com>
2016-08-23 14:57:05 -0400

    first demo mocks for users

--- mocks/users.js
+++ mocks/users.js
@@ -8,100 +8,34 @@ var GetResults = {
     name: 'GetResults',
     mockRoute: 'users',
     testScope: 'success', //success | fail | error
-    testScenario: 'singleResult',
+    testScenario: 'noResults',
     jsonTemplate: [
         {
-            multipleResults: function () {
-                return '[{{#repeat 18}}' +
-                    '{' +
-                    '   "id":654461,' +
-                    '   "fileTypeId":{{number 100}},' +
-                    '   "fileTypeName":"{{company}}",' +
-                    '   "fileTypeDescription":"{{company}}",' +
-                    '   "drawerId":{{number 100}},' +
-                    '   "drawerName":"{{company}}",' +
-                    '   "drawerDescription":"{{company}}",' +
-                    '   "notesId":{{number 100}},' +
-                    '   "description":"{{company}}",' +
-                    '   "fileNumberPart1":"{{company}}",' +
-                    '   "fileNumberPart2":"",' +
-                    '   "fileNumberPart3":"pt3",' +
-                    '   "isTemporary":{{boolean}},' +
-                    '   "isDeleted":{{boolean}},' +
-                    '   "dateLastOpened":"2015-06-01T17:41:20.3992519-04:00",' +
-                    '   "lastModified":"2015-06-01T17:41:20.3992519-04:00",' +
-                    '   "dateCreated":"2015-06-01T17:41:20.3992519-04:00",' +
-                    '   "attributes":[{{#repeat 5}}' +
-                    '       {' +
-                    '           "name":"NameStr{{number 10}}",' +
-                    '           "value":{' +
-                    '           }' +
-                    '       } {{/repeat}}' +
-                    '   ]' +
-                    '} {{/repeat}}' +
-                    ']';
-            }
-        },
-        {
             singleResult: function () {
-                return '[{{#repeat 1}}' +
-                    '{' +
-                    '   "id":654461,' +
-                    '   "fileTypeId":{{number 100}},' +
-                    '   "fileTypeName":"{{company}}",' +
-                    '   "fileTypeDescription":"{{company}}",' +
-                    '   "drawerId":{{number 100}},' +
-                    '   "drawerName":"{{company}}",' +
-                    '   "drawerDescription":"{{company}}",' +
-                    '   "notesId":{{number 100}},' +
-                    '   "description":"{{company}}",' +
-                    '   "fileNumberPart1":"{{company}}",' +
-                    '   "fileNumberPart2":"",' +
-                    '   "fileNumberPart3":"pt3",' +
-                    '   "isTemporary":{{boolean}},' +
-                    '   "isDeleted":{{boolean}},' +
-                    '   "dateLastOpened":"2015-06-01T17:41:20.3992519-04:00",' +
-                    '   "lastModified":"2015-06-01T17:41:20.3992519-04:00",' +
-                    '   "dateCreated":"2015-06-01T17:41:20.3992519-04:00",' +
-                    '   "attributes":[{{#repeat 5}}' +
-                    '       {' +
-                    '           "name":"NameStr{{number 10}}",' +
-                    '           "value":{' +
-                    '           }' +
-                    '       } {{/repeat}}' +
-                    '   ]' +
-                    '} {{/repeat}}' +
-                    ']';
+                var users = {'data': [
+                    {
+                        'displayName': 'User 1',
+                        'displayEmail': 'user1@email.com',
+                        'bookmarked': false
+                    }]};
+                return JSON.stringify(users);
             }
         },
         {
-            twentyResultsWithUniqueIds: function () {
-                var files = [];
-
-                for (var i = 0; i < 20; i++) {
-                    var file = {
-                        id: i,
-                        fileTypeId: 100,
-                        fileTypeName: "No FileType",
-                        fileTypeDescription: "No FileType",
-                        drawerId: 100,
-                        drawerName: "No Drawer",
-                        drawerDescription: "No Drawer",
-                        notesId: 1,
-                        description: 'File ' + i,
-                        fileNumberPart1: 'File ' + i,
-                        fileNumberPart2: "",
-                        fileNumberPart3: "",
-                        isTemporary: false,
-                        isDeleted: false,
-                        dateLastOpened: "2015-06-01T17:41:20.3992519-04:00",
-                        lastModified: "2015-06-01T17:41:20.3992519-04:00",
-                        dateCreated: "2015-06-01T17:41:20.3992519-04:00"
-                    };
-                    files.push(file);
-                }
-
-                return JSON.stringify(files);
+            multipleResults: function () {
+                return '{"data": [{{#repeat 180}}' +
+                    '{' +
+                    '   "displayName":"{{company}}",' +
+                    '   "displayEmail":"{{company}}@{{company}}.com",' +
+                    '   "bookmarked":"{{boolean}}"' +
+                    '} {{/repeat}}' +
+                    ']}';
+            }
+        },
+        {
+            noResults: function () {
+                var users = {'data': []};
+                return JSON.stringify(users);
             }
         }
     ]
