8cc6a53409c88665c55b3b175a1f9db34558fc2a
Andrew Chumich <andrewchumich@gmail.com>
2016-08-25 14:15:42 -0600

    change name of function

--- src/app/users/users.reducer.spec.ts
+++ src/app/users/users.reducer.spec.ts
@@ -1,7 +1,7 @@
 import { UsersReducer, SelectedUserReducer, LoadingUserReducer, UserErrorsReducer, IUser, ADD_USERS, DELETE_USER, CREATE_USERS, UPDATE_USERS } from './users';
 
 describe('UsersReducer::', ()=> {
-    var makeDummyUser = ({ userID=0, userName='' }: { userID?: number, userName?: string }): IUser => {
+    var makeUser = ({ userID=0, userName='' }: { userID?: number, userName?: string }): IUser => {
         return {
             userName: userName,
             email: '',
@@ -20,9 +20,9 @@ describe('UsersReducer::', ()=> {
 
     it('ADD_USERS adds the provided payload', ()=>{
         let dummyUsers: IUser[] = [
-            makeDummyUser({ userID: 0 }),
-            makeDummyUser({ userID: 1 }),
-            makeDummyUser({ userID: 2 }),
+            makeUser({ userID: 0 }),
+            makeUser({ userID: 1 }),
+            makeUser({ userID: 2 }),
         ];
         let addItem = UsersReducer(undefined, {type: ADD_USERS, payload: dummyUsers});
         expect(addItem).toEqual(dummyUsers);
@@ -30,25 +30,25 @@ describe('UsersReducer::', ()=> {
 
     it('DELETE_USER removes the provided payload', () => {
         let initialUsers: IUser[] = [
-            makeDummyUser({ userID: 0 }),
-            makeDummyUser({ userID: 1 }),
-            makeDummyUser({ userID: 2 }),
+            makeUser({ userID: 0 }),
+            makeUser({ userID: 1 }),
+            makeUser({ userID: 2 }),
         ];
-        let removeItem = UsersReducer(initialUsers, { type: DELETE_USER, payload: makeDummyUser({ userID: 1 }) });
+        let removeItem = UsersReducer(initialUsers, { type: DELETE_USER, payload: makeUser({ userID: 1 }) });
         expect(removeItem).not.toEqual(initialUsers);
         expect(removeItem).toEqual(initialUsers.filter((user: IUser) => user.userID !== 1));
     });
 
     it('CREATE_USERS appends users to the users list', () => { 
         let initialUsers: IUser[] = [
-            makeDummyUser({ userID: 0 }),
-            makeDummyUser({ userID: 1 }),
-            makeDummyUser({ userID: 2 }),
+            makeUser({ userID: 0 }),
+            makeUser({ userID: 1 }),
+            makeUser({ userID: 2 }),
         ];
 
         let usersToAdd: IUser[] = [
-            makeDummyUser({ userID: 3 }),
-            makeDummyUser({ userID: 4 }),
+            makeUser({ userID: 3 }),
+            makeUser({ userID: 4 }),
         ];
 
         let createUsersState = UsersReducer(initialUsers, { type: CREATE_USERS, payload: usersToAdd});
@@ -59,15 +59,15 @@ describe('UsersReducer::', ()=> {
 
     it('UPDATE_USERS should update the user with the same id', () => {
         let initialUsers: IUser[] = [
-            makeDummyUser({ userID: 0 }),
-            makeDummyUser({ userID: 1, userName: 'InitialName1' }),
-            makeDummyUser({ userID: 2, userName: 'InitialName2' }),
-            makeDummyUser({ userID: 3 }),
+            makeUser({ userID: 0 }),
+            makeUser({ userID: 1, userName: 'InitialName1' }),
+            makeUser({ userID: 2, userName: 'InitialName2' }),
+            makeUser({ userID: 3 }),
         ];
 
         let usersToUpdate: IUser[] = [
-            makeDummyUser({ userID: 1, userName: 'NewName1' }),
-            makeDummyUser({ userID: 2, userName: 'NewName2' }),
+            makeUser({ userID: 1, userName: 'NewName1' }),
+            makeUser({ userID: 2, userName: 'NewName2' }),
         ];
         
         let updateUsersState = UsersReducer(initialUsers, { type: UPDATE_USERS, payload: usersToUpdate });
