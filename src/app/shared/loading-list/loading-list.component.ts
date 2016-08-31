419edb473f4af669ebcf5b3a01e533dcee5cbf3f
Jeremy Zerr <jeremy@zerrtech.com>
2016-08-31 14:36:16 -0600

    Initial loading animation for list

--- src/app/shared/loading-list/loading-list.component.ts
+++ src/app/shared/loading-list/loading-list.component.ts
@@ -1,20 +1,70 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import { Component, Input, OnChanges, trigger, state, style, transition, animate } from '@angular/core';
 
 @Component({
     selector: 'app-loading-list',
     template: require('./loading-list.component.html'),
     styles: [require('./loading-list.component.less')],
-    
+    animations: [
+      trigger('loadingState', [
+        state('loading', style({ backgroundColor: '#0000ff' })),
+        state('success', style({ backgroundColor: '#00ff00' })),
+        state('error', style({ backgroundColor: '#ff0000' }))
+      ]),
+      trigger('loadingHeight', [
+        state('notloading', style({ transform: 'translateX(100%)' })),
+        state('loading', style({  backgroundColor: '#0000ff', transform: 'translateX(0)' })),
+        state('success', style({ backgroundColor: '#00ff00', transform: 'translateX(100%)' })),
+        state('error', style({ backgroundColor: '#ff0000', transform: 'translateX(0)' })),
+        transition('* => loading', [
+          style({ transform: 'translateX(-100%)' }),
+          animate('250ms ease-in')
+        ]),
+        transition('loading => success', animate('250ms ease-out'))
+      ])      
+    ]
 })
 export class LoadingListComponent implements OnChanges {
   @Input() isLoading: boolean;
   @Input() delay: number;
+  @Input() isSuccess: boolean;
+  @Input() isError: boolean;
   showLoading: boolean = false;
   timeoutId: any = undefined;
+  /**
+   * Loading status represents the current status based on
+   * the isLoading, isSuccess, and isError inputs
+   * It can be one of the following values:
+   * notloading
+   * loading
+   * success
+   * error
+   * 
+   * @type {string}
+   */
+  loadingState: string = 'notloading';
   constructor() {
   };
 
   ngOnChanges(changes) {
+    console.log('in loading comp before load:', this.loadingState);
+    if (changes.isLoading || changes.isSuccess || changes.isError) {
+      // populate the loadingStatus used to determine animations
+      if (this.isLoading) {
+        this.loadingState = "loading";
+      } else if (this.isSuccess) {
+        this.loadingState = "success";
+      } else if (this.isError) {
+        this.loadingState = "error";
+      } else {
+        this.loadingState = "notloading";
+      }
+    }
+
+
+    console.log('loading state in loading list:', this.loadingState);
+
+
+
     if (changes.isLoading.previousValue !== true && changes.isLoading.currentValue === true) {
       this.timeoutId = setTimeout(() => {
         this.showLoading = true;
--- src/app/users/components/users.list.ts
+++ src/app/users/components/users.list.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter, trigger, state, style, transition, animate } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, trigger, state, style, transition, animate } from '@angular/core';
 import { IUser } from '../users';
 
 //-------------------------------------------------------------------
@@ -17,10 +17,16 @@ import { IUser } from '../users';
             transition('* => void', [
                 animate(250, style({transform: 'translateX(100%)'}))
             ])
+        ]),
+        trigger('loadingState', [
+            state('notloading', style({ top: '140px'})),
+            state('loading', style({ top: '205px'})),
+            transition('loading => *', animate('250ms ease-in')),
+            transition('* => loading', animate('250ms ease-in'))
         ])
     ]
 })
-export class UsersList {
+export class UsersList implements OnChanges {
     @Input() items: IUser[];
     @Input() isLoading: boolean;
     @Input() isError: boolean;
@@ -28,6 +34,8 @@ export class UsersList {
     @Output() selected = new EventEmitter();
     @Output() deleted = new EventEmitter();
     animationState: string;
+    isSuccess: boolean = true; // $$$ fix this
+    loadingState: string = 'notloading';
 
     constructor(){
         this.animationState = 'in';
@@ -35,5 +43,22 @@ export class UsersList {
     clearError() {
         this.onClearError.emit({});
     }
+
+  ngOnChanges(changes) {
+    console.log('in users list before load:', this.loadingState);
+    if (changes.isLoading || changes.isSuccess || changes.isError) {
+      // populate the loadingStatus used to determine animations
+      if (this.isLoading) {
+        this.loadingState = "loading";
+      } else if (this.isSuccess) {
+        this.loadingState = "success";
+      } else if (this.isError) {
+        this.loadingState = "error";
+      } else {
+        this.loadingState = "notloading";
+      }
+    }
+    console.log('loadingState in users list:', this.loadingState);
+  }
 
 }
