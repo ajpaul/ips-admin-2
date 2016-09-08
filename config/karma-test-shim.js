4b256de8a7cf66344fd78957694e6d5442da1938
sxd15 <scott.davis@acuitybrands.com>
2016-09-08 16:54:40 -0400

    commented out es7 stuff - added in es6 & refllect-metadata - left in ts helpers against kelly's advice

--- config/karma-test-shim.js
+++ config/karma-test-shim.js
@@ -1,15 +1,17 @@
 Error.stackTraceLimit = Infinity;
 
-require('ts-helpers');
-require('core-js/es7/reflect');
+ require('ts-helpers');
+// require('core-js/es7/reflect');
+require('core-js/es6');
+require('reflect-metadata');
 require('zone.js/dist/zone');
 require('zone.js/dist/long-stack-trace-zone');
 require('zone.js/dist/jasmine-patch');
 require('zone.js/dist/async-test');
 require('jasmine-ajax');
 
-var appContext = require.context('../src', true, /\.spec\.ts/);
-
+var appContext = require.context('../src/app', false, /\.spec\.ts/);
+console.log(appContext.keys());
 appContext.keys().forEach(appContext);
 
 var testing = require('@angular/core/testing');
@@ -19,3 +21,4 @@ testing.TestBed.initTestEnvironment(
     browser.BrowserDynamicTestingModule,
     browser.platformBrowserDynamicTesting()
 );
+console.log('789');
